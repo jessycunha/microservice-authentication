1a1ef73e97e941197e2fd62c75d6d4330a5bef9b
Cunha <jessic4cunha@gmail.com>
2022-02-12 13:59:06 -0300

    corrections

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = 'postgres://kgybryns:QYmrOJgna0yNrZxQLVAlzg6XDIbM8tXR@motty.db.elephantsql.com/kgybryns';
+const connectionString = '';
 
 const db = new Pool({ connectionString });
 
